24e5af138123c6b2d1e2de86a675165b86fd9def
matheuscoimbraunipam <104603026+matheuscoimbraunipam@users.noreply.github.com>
2024-12-03 22:51:52 +0000

    Pratica 02

---
+++ pratica02/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using pratica02.Models;
+
+namespace pratica02.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Carro> Carros { get; set; }
+    }
+}
+        
